21fe95abb1003fe65aaf016e607aa2702a5cfc30
Natalia Justicia <nataliajusticia@gmail.com>
2024-02-21 16:12:42 +0100

    Add VisualFieldEditButton component

---
+++ src/components/nodehive/visualeditor/VisualFieldEditButton.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
+
+export default function VisualFieldEditButton({
+  label,
+  type,
+  field,
+  nodeId,
+  lang,
+}) {
+  const pathname = usePathname();
+  const [isInIframe, setIsInIframe] = useState(false);
+
+  const editComponent = (e) => {
+    e.preventDefault();
+
+    // TODO: Add language information. To edit the content in the correct language.
+    // Backend implementation is not ready yet.
+    window.parent.postMessage(
+      {
+        type: type,
+        field: field,
+        id: nodeId,
+        pathname: pathname,
+        lang: lang,
+      },
+      '*'
+    );
+  };
+
+  useEffect(() => {
+    const inIframe = window.self !== window.top;
+    setIsInIframe(inIframe);
+  }, []);
+
+  if (!isInIframe) {
+    return null;
+  }
+
+  return (
+    <button
+      onClick={editComponent}
+      className="absolute right-2 top-2 rounded-lg bg-neutral-900 p-2 text-xs font-bold text-white transition-colors hover:bg-neutral-700"
+    >
+      {label}
+    </button>
+  );
+}
