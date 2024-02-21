896d7acff092bd65f7bf860bcf7d6ec6f709e493
Natalia Justicia <nataliajusticia@gmail.com>
2024-02-21 14:01:01 +0100

    Update Header styling

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import HeaderNavigation from './header/header-navigation';
 
 export default async function Header() {
   return (
-    <header className="sticky top-0 bg-white shadow-sm">
+    <header className="sticky top-0 z-10 bg-white shadow-sm">
       <div className="container mx-auto flex items-center justify-between px-4 py-8 md:px-8">
         <HeaderLogo />
 
