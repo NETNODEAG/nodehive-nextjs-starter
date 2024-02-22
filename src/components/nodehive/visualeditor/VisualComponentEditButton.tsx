5213a924af65885d1b929c6bdbcced61aaae0ed7
Natalia Justicia <nataliajusticia@gmail.com>
2024-02-22 14:22:51 +0100

    Add refreshPage function to handle message event

--- src/components/nodehive/visualeditor/VisualComponentEditButton.tsx
+++ src/components/nodehive/visualeditor/VisualComponentEditButton.tsx
@@ -32,10 +32,45 @@ export default function VisualComponentEditButton({
   useEffect(() => {
     const inIframe = window.self !== window.top;
     setIsInIframe(inIframe);
+
+    const handleMessage = (event) => {
+      if (event.data === 'reloadFrame') {
+        refreshPage();
+      }
+    };
+
+    window.addEventListener('message', handleMessage);
+
+    return () => {
+      window.removeEventListener('message', handleMessage);
+    };
   }, []);
 
   if (!isInIframe) {
     return null;
+  }
+
+  async function refreshPage() {
+    let success = false;
+    let error = null;
+
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_FRONTEND_BASE_URL}/api/nodehive/revalidate?path=${pathname}`
+      );
+
+      if (!response.ok) {
+        error = 'Error revalidating the page';
+      } else {
+        success = true;
+        location.reload();
+      }
+    } catch (error) {
+      console.error(error);
+      error = error?.message;
+    }
+
+    return { success, error };
   }
 
   return (
