285899f052c8c36652f33281d6962c3c85ce34ce
Natalia Justicia <nataliajusticia@gmail.com>
2024-02-22 15:22:25 +0100

    Update SmartActionsButton styling

--- src/components/nodehive/SmartActionsButton.tsx
+++ src/components/nodehive/SmartActionsButton.tsx
@@ -4,7 +4,7 @@ import SmartActionsVisualEditor from './smart-actions/SmartActionsVisualEditor';
 
 export default function SmartActionsButton() {
   return (
-    <div className="fixed bottom-10 left-1/2 -translate-x-1/2">
+    <div className="fixed bottom-10 left-1/2 z-50 -translate-x-1/2">
       <div className="rounded-full bg-neutral-900 p-2 text-sm font-bold text-white shadow-[0_8px_40px_rgba(0,0,0,0.25)] shadow-neutral-900/30 backdrop-blur-2xl ">
         <ul className="flex items-center gap-2">
           <li>
