91a32ab495e1da763eb3981518df65bd28d0d3a7
lukas.fischer <lukas.fischer@netnode.ch>
2024-04-08 23:27:38 +0200

    improved connector.js

--- src/nodehive/connector.js
+++ src/nodehive/connector.js
@@ -1,57 +1,38 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
-import { EditIcon } from '@/lib/icons';
-
-export default function Connector({}) {
-  const pathname = usePathname();
-  const [isInIframe, setIsInIframe] = useState(false);
+export default function Connector() {
+  const pathname = usePathname(); // Use the usePathname hook at the top level
+  const [isInIframe, setIsInIframe] = useState(false); // State to check if the component is in an iframe
+  const [isReloading, setIsReloading] = useState(false); // State to manage the reloading state
 
   useEffect(() => {
-    const inIframe = window.self !== window.top;
-    setIsInIframe(inIframe);
+    setIsInIframe(window.self !== window.top);
 
-    const handleMessage = (event) => {
+    const handleMessage = async (event) => {
       if (event.data === 'reloadFrame') {
-        refreshPage();
+        setIsReloading(true); // Start showing the overlay
+
+        console.log(pathname);
+        await fetch(
+          `${process.env.NEXT_PUBLIC_FRONTEND_BASE_URL}/api/nodehive/revalidate?path=${pathname}`
+        );
+
+        location.reload();
       }
     };
 
     window.addEventListener('message', handleMessage);
+    return () => window.removeEventListener('message', handleMessage);
+  }, [pathname]); // Include pathname and router in the dependency array
 
-    return () => {
-      window.removeEventListener('message', handleMessage);
-    };
-  }, []);
-
-  if (!isInIframe) {
-    return null;
-  }
-
-  async function refreshPage() {
-    let success = false;
-    let error = null;
-
-    try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_FRONTEND_BASE_URL}/api/nodehive/revalidate?path=${pathname}`
-      );
-
-      if (!response.ok) {
-        error = 'Error revalidating the page';
-      } else {
-        success = true;
-        location.reload();
-      }
-    } catch (error) {
-      console.error(error);
-      error = error?.message;
-    }
-
-    return { success, error };
-  }
-
-  return <></>;
+  return <>{isInIframe && isReloading && <SavingMessage />}</>;
 }
+
+const SavingMessage = () => (
+  <div className="fixed inset-0 z-50 flex items-center justify-center bg-teal-900 bg-opacity-50 transition-all">
+    <div className="text-3xl font-semibold text-white">Saving...</div>
+  </div>
+);
