e127628d8d228419521a4dbbe11ee1af2f55961d
Natalia Justicia <nataliajusticia@gmail.com>
2024-05-03 10:45:25 +0200

    NH-223 - Add user account page with logout functionality

--- src/app/nodehive/account/page.tsx
+++ src/app/nodehive/account/page.tsx
@@ -1,12 +1,40 @@
-import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+import { logout } from '@/actions/_auth';
+import { getUser } from '@/actions/_user';
 
-export default function Page() {
-  const cookieStore = cookies();
-  const user = cookieStore.get('user')?.value;
+export default async function Page() {
+  const { user } = await getUser();
+
+  if (!user) {
+    redirect('/nodehive/login');
+  }
+
+  {/* @ts-ignore */}
+  const name = user?.name?.[0]?.value;
+  {/* @ts-ignore */}
+  const email = user?.mail?.[0]?.value;
 
   return (
     <section className="space-y-8">
       <h1 className="text-4xl font-bold sm:text-6xl">Account</h1>
+
+      <div className="space-y-4">
+        <ul>
+          <li>
+            <strong>Username:</strong> {name}
+          </li>
+          <li>
+            <strong>Email:</strong> {email}
+          </li>
+          <li className="mt-8">
+            <form action={logout}>
+              <button type="submit" className="text-sm font-bold uppercase">
+                Logout
+              </button>
+            </form>
+          </li>
+        </ul>
+      </div>
     </section>
   );
 }
