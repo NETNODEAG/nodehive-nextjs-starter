258cff5edad1a976428ef4d0c0cbd089105bbf7d
Natalia Justicia <nataliajusticia@gmail.com>
2024-05-08 15:13:35 +0200

    refactor: Update paragraph-text component to include image rendering

--- src/components/paragraph/text/paragraph-text.tsx
+++ src/components/paragraph/text/paragraph-text.tsx
@@ -22,29 +22,27 @@ export default function ParagraphText({ paragraph }: ParagraphTextProps) {
     >
       <div>
         {byline && <h2 className="text-xl">{byline}</h2>}
+
         <h2 className="mb-4 mt-2 text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl">
           {title}
         </h2>
+
         {text && (
           <FormattedText html={text?.processed} className="prose max-w-prose" />
         )}
       </div>
 
-      {image && (
-        <div className="lg:pl-20">
-          {imageUrl && (
-            <div className="max-w-xs px-2.5 lg:max-w-none">
-              <Image
-                src={imageUrl}
-                priority={true}
-                alt=""
-                width={300}
-                height={300}
-                sizes="(min-width: 1024px) 32rem, 20rem"
-                className="aspect-square rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
-              />
-            </div>
-          )}
+      {imageUrl && (
+        <div className="max-w-xs px-2.5 lg:max-w-none">
+          <Image
+            src={imageUrl}
+            priority={true}
+            alt=""
+            width={300}
+            height={300}
+            sizes="(min-width: 1024px) 32rem, 20rem"
+            className="aspect-square rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
+          />
         </div>
       )}
     </section>
