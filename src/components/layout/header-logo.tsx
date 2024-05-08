93613daef9edc6bf71b332f5ce84155f951acb26
Natalia Justicia <nataliajusticia@gmail.com>
2024-05-08 12:05:03 +0200

    fix: header logo fragment

--- src/components/layout/header-logo.tsx
+++ src/components/layout/header-logo.tsx
@@ -1,13 +1,19 @@
 import { createServerClient } from '@/nodehive/client';
+import { DrupalJsonApiParams } from 'drupal-jsonapi-params';
 
 import Fragment from '@/components/fragment/Fragment';
 
 export default async function HeaderLogo() {
   const client = createServerClient();
 
+  const apiParams = new DrupalJsonApiParams();
+  const queryString = apiParams.addInclude(['field_logo']);
+
   const fragment = await client.getFragment(
     '79261be4-ca1a-4959-878f-b07fe4ed3e18',
-    'space_logo'
+    'space_logo',
+    'en',
+    queryString
   );
 
   return <Fragment fragment={fragment?.data} />;
